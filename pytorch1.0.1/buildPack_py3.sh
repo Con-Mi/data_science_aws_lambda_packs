6816509ada3648c89908420ffc7863f5ce177599
Con-MI <aspris.marios@gmail.com>
2019-02-15 10:26:24 +0100

    Included shell scripts for creating Pytorch Lambda Pack

---
+++ pytorch1.0.1/buildPack_py3.sh
@@ -0,0 +1,3 @@
+docker pull amazonlinux:1
+docker run -v $(pwd):/outputs --name lambdapackgen-pytorch -d amazonlinux:1 tail -f /dev/null
+docker exec -i -t lambdapackgen-pytorch /bin/bash /outputs/buildPack_py3.sh
