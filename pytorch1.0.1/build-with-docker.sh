67f2c007f9ae84734accf5b06e936f5abd6e98d5
Marios Aspris <aspris.marios@gmail.com>
2019-04-05 16:46:10 +0200

    amazonlinux image 1 breaks the build

--- pytorch1.0.1/build-with-docker.sh
+++ pytorch1.0.1/build-with-docker.sh
@@ -1,3 +1,3 @@
-docker pull amazonlinux:2
-docker run -v $(pwd):/outputs --name lambdapackgen-pytorch -d amazonlinux:2 tail -f /dev/null
+docker pull amazonlinux:1
+docker run -v $(pwd):/outputs --name lambdapackgen-pytorch -d amazonlinux:1 tail -f /dev/null
 docker exec -i -t lambdapackgen-pytorch /bin/bash /outputs/buildPack_py3.sh
